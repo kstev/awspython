853c819bd9d0b84db5e86f6e05cd5b7f8886d88b
Kevin Stevens <kevin@mbpro.local>
2020-02-06 16:57:50 -0600

    cleanup

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,43 +1,66 @@
-import sys
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Webotron: Deploy websites to AWS.
+
+Webotron allows you to create static websites
+- Configure AWS S3 buckets
+ - Create
+ - Enable static website hosting on a bucket
+ - Sync a local directory to a bucket
+- Configure DNS with AWS Route 53
+- Configure a CDN and SSL with AWS CloudFront
+"""
+
+from pathlib import Path
+import mimetypes
+
 import boto3
 import click
 from botocore.exceptions import ClientError
-from pathlib import Path
-import mimetypes
 
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
 
+
 @click.group()
 def cli():
-    "Webotron deploys websites to AWS"
+    """Webotron deploys websites to AWS."""
     pass
+
 
 @cli.command('list-buckets')
 def list_buckets():
-    "List all s3 buckets"
+    """List all s3 buckets."""
     for bucket in s3.buckets.all():
         print(bucket)
+
 
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    "List objects in an s3 bucket"
+    """List objects in an s3 bucket."""
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
+
 
 @cli.command('setup-bucket')
 @click.argument('bucket')
 def setup_bucket(bucket):
-    "Create website enabled bucket"
+    """Create website enabled bucket."""
     s3_bucket = None
     try:
-        s3_bucket = s3.create_bucket(Bucket=bucket, CreateBucketConfiguration={'LocationConstraint': session.region_name})
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+        s3_bucket = s3.create_bucket(
+            Bucket=bucket,
+            CreateBucketConfiguration={
+                'LocationConstraint': session.region_name
+                }
+            )
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
             s3_bucket = s3.Bucket(bucket)
         else:
-            raise e
+            raise error
     policy = '''
     {
         "Version": "2012-10-17",
@@ -54,37 +77,45 @@ def setup_bucket(bucket):
     ''' % s3_bucket.name
     pol = s3_bucket.Policy()
     pol.put(Policy=policy.strip())
-    ws = s3_bucket.Website()
-    ws.put(WebsiteConfiguration={'ErrorDocument': {'Key': 'error.html'}, 'IndexDocument': {'Suffix': 'index.html'}})
-    #s3_bucket.upload_file('index.html', 'index.html', ExtraArgs={'ContentType': 'text/html'})
-    url = 'http://%s.s3-website.%s.amazonaws.com' % (s3_bucket.name,session.region_name)
+    s3_bucket.Website().put(WebsiteConfiguration={
+        'ErrorDocument': {'Key': 'error.html'},
+        'IndexDocument': {'Suffix': 'index.html'}
+        })
+    url = 'http://%s.s3-website.%s.amazonaws.com' % (s3_bucket.name, session.region_name)
     return url
 
+
 def upload_file(s3_bucket, path, key):
+    """Upload a file to S3."""
     content_type = mimetypes.guess_type(key)[0] or 'text/plain'
     s3_bucket.upload_file(
         path,
         key,
         ExtraArgs={
-            'ContentType': 'text/html'
+            'ContentType': content_type
         }
     )
+
 
 @cli.command('sync')
 @click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
 def sync(pathname, bucket):
-    "Sync contents of PATH to BUCKET"
+    """Sync contents of PATH to BUCKET."""
     s3_bucket = s3.Bucket(bucket)
 
     root = Path(pathname).expanduser().resolve()
+
     def handle_directory(target):
+        """Iterate through directory to upload."""
         for p in target.iterdir():
-            if p.is_dir(): handle_directory(p)
-            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
-    
+            if p.is_dir():
+                handle_directory(p)
+            if p.is_file():
+                upload_file(s3_bucket, str(p), str(p.relative_to(root)))
+
     handle_directory(root)
-    
+
 
 if __name__ == '__main__':
     cli()
